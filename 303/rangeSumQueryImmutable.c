e2a00c8a0e0c32776ecf75049800276b20651cec
1216114309zpf <1216114309@qq.com>
2018-01-05 06:48:25 -0800

    preprocessor

---
+++ 303/rangeSumQueryImmutable.c
@@ -0,0 +1,16 @@
+class NumArray {
+public:
+    int *sum;
+    NumArray(vector<int> nums) {
+        sum=new int[nums.size()+1];
+        sum[0]=0;
+        for(int i=1;i<nums.size()+1;++i){
+            sum[i]=sum[i-1]+nums[i-1];
+        }
+    }
+    
+    int sumRange(int i, int j) {
+        return sum[j+1]-sum[i];
+    }
+};
+
