9830a29c87943ddea6a7e031cb8d3c3f291ad6bf
1216114309zpf <1216114309@qq.com>
2018-03-30 21:09:30 -0700

    heap

---
+++ 378/KthSmallestElementInASortedMatrix.c
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<int,vector<int>,greater<int>> que;
+        for(int i=0;i<matrix.size();++i){
+            for(int j=0;j<matrix[i].size();++j){
+                que.push(matrix[i][j]);
+            }
+        }
+        
+        while(k-1){
+            que.pop();
+            --k;
+        }
+        
+        return que.top();
+    }
+};
